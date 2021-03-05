5ada90f360238df0053276fd342067aeab0c4489
장상훈(협력사) <ubecha@poscoict.com>
2021-03-05 09:13:58 +0900

    Test1 수정

--- src/Test1.java
+++ src/Test1.java
@@ -2,10 +2,4 @@ public class Test1 {
     public Test1() {
         System.out.println("Test1 Contructor");
     }
-    public void test() {
-        System.out.println("test method call");
-    }
-    public void test1() {
-        System.out.println("test1 called");
-    }
 }
